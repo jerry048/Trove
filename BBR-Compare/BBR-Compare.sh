042941628314374db0839b704b4bd2f42c5146f3
jerry048 <61510650+jerry048@users.noreply.github.com>
2024-07-24 20:51:50 +0800

    BBR Compare Update

--- BBR-Compare/BBR-Compare.sh
+++ BBR-Compare/BBR-Compare.sh
@@ -1,76 +1,80 @@
 #!/bin/bash
 
 # Server IP addresses or hostnames
-SERVER="105.235.237.2"
+SERVER="speedtest.lax12.us.leaseweb.net"
 
-# Ports (make sure these are open and listening on the servers)
+# IPv4 or IPv6
+IP_VERSION=4
+
+# Ports
 PORT1=5201
 PORT2=5202
 PORT3=5203
-PORT4=5204
-PORT5=5205
+
+# Algorithm
+Algo1=cubic
+Algo2=reno
+Algo3=bbr
 
 # Duration of the test in seconds
 DURATION=30
 
+# Function to display a progress bar
+progress_bar() {
+    local duration=$1
+    local elapsed=0
+    while [ $elapsed -lt $duration ]; do
+        printf "\rProgress: [%-30s] %d%%" $(printf "#%.0s" $(seq 1 $((elapsed * 30 / duration)))) $((elapsed * 100 / duration))
+        sleep 1
+        elapsed=$((elapsed + 1))
+    done
+    printf "\rProgress: [%-30s] %d%%\n" $(printf "#%.0s" $(seq 1 30)) 100
+}
+
+echo "Testing $Algo1 vs $Algo2 vs $Algo3 on $SERVER"
 # Run iperf3 with cubic congestion control
-iperf3 -c $SERVER -p $PORT1 -C cubic -t $DURATION > /tmp/cubic_results.txt &
+iperf3 -c $SERVER -p $PORT1 -C $Algo1 -t $DURATION -$IP_VERSION > /tmp/${Algo1}_results.txt &
 PID1=$!
 
 # Run iperf3 with bbr congestion control
-iperf3 -c $SERVER -p $PORT2 -C bbr -t $DURATION > /tmp/bbr_results.txt &
+iperf3 -c $SERVER -p $PORT2 -C $Algo2 -t $DURATION -$IP_VERSION > /tmp/${Algo2}_results.txt &
 PID2=$!
 
-# Run iperf3 with bbr congestion control
-iperf3 -c $SERVER -p $PORT3 -C bbrw -t $DURATION > /tmp/bbrw_results.txt &
+# Run iperf3 with reno congestion control
+iperf3 -c $SERVER -p $PORT3 -C $Algo3 -t $DURATION -$IP_VERSION > /tmp/${Algo3}_results.txt &
 PID3=$!
 
-# Run iperf3 with bbr congestion control
-iperf3 -c $SERVER -p $PORT4 -C bbry -t $DURATION > /tmp/bbry_results.txt &
-PID4=$!
+# Show progress bar for the duration of the tests
+progress_bar $DURATION &
 
-# Run iperf3 with bbr congestion control
-iperf3 -c $SERVER -p $PORT5 -C bbrx -t $DURATION > /tmp/bbrx_results.txt &
-PID5=$!
-
-
-# Wait for both processes to finish
-wait $PID1 $PID2 $PID3 $PID4 $PID5
+# Wait for all processes to finish
+wait $PID1 $PID2 $PID3
 
 ##DISPLAY RESULTS
-#Throughput
-cubic=$(cat /tmp/cubic_results.txt | grep "sender" | awk '{print $7}')
-bbr=$(cat /tmp/bbr_results.txt | grep "sender" | awk '{print $7}')
-bbrw=$(cat /tmp/bbrw_results.txt | grep "sender" | awk '{print $7}')
-bbry=$(cat /tmp/bbry_results.txt | grep "sender" | awk '{print $7}')
-bbrx=$(cat /tmp/bbrx_results.txt | grep "sender" | awk '{print $7}')
-#Unit
-cubic_u=$(cat /tmp/cubic_results.txt | grep "sender" | awk '{print $8}')
-bbr_u=$(cat /tmp/bbr_results.txt | grep "sender" | awk '{print $8}')
-bbrw_u=$(cat /tmp/bbrw_results.txt | grep "sender" | awk '{print $8}')
-bbry_u=$(cat /tmp/bbry_results.txt | grep "sender" | awk '{print $8}')
-bbrx_u=$(cat /tmp/bbrx_results.txt | grep "sender" | awk '{print $8}')
-#Retransmissions
-cubic_retrans=$(cat /tmp/cubic_results.txt | grep "sender" | awk '{print $9}')
-bbr_retrans=$(cat /tmp/bbr_results.txt | grep "sender" | awk '{print $9}')
-bbrw_retrans=$(cat /tmp/bbrw_results.txt | grep "sender" | awk '{print $9}')
-bbry_retrans=$(cat /tmp/bbry_results.txt | grep "sender" | awk '{print $9}')
-bbrx_retrans=$(cat /tmp/bbrx_results.txt | grep "sender" | awk '{print $9}')
+# Throughput
+test1=$(cat /tmp/${Algo1}_results.txt | grep "sender" | awk '{print $7}')
+test2=$(cat /tmp/${Algo2}_results.txt | grep "sender" | awk '{print $7}')
+test3=$(cat /tmp/${Algo3}_results.txt | grep "sender" | awk '{print $7}')
+
+# Unit
+test1_u=$(cat /tmp/${Algo1}_results.txt | grep "sender" | awk '{print $8}')
+test2_u=$(cat /tmp/${Algo2}_results.txt | grep "sender" | awk '{print $8}')
+test3_u=$(cat /tmp/${Algo3}_results.txt | grep "sender" | awk '{print $8}')
+
+# Retransmissions
+test1_retrans=$(cat /tmp/${Algo1}_results.txt | grep "sender" | awk '{print $9}')
+test2_retrans=$(cat /tmp/${Algo2}_results.txt | grep "sender" | awk '{print $9}')
+test3_retrans=$(cat /tmp/${Algo3}_results.txt | grep "sender" | awk '{print $9}')
 
 # Display results in a table
 echo "Results"
 echo "---------------------------------"
 echo "Congestion Control | Throughput | Retransmissions"
 echo "---------------------------------"
-echo "Cubic              | $cubic $cubic_u | $cubic_retrans"
-echo "BBR                | $bbr $bbr_u | $bbr_retrans"
-echo "BBRw               | $bbrw $bbrw_u | $bbrw_retrans"
-echo "BBRy               | $bbry $bbry_u | $bbry_retrans"
-echo "BBRx               | $bbrx $bbrx_u | $bbrx_retrans"
+echo "$Algo1             | $test1 $test1_u | $test1_retrans"
+echo "$Algo2             | $test2 $test2_u | $test2_retrans"
+echo "$Algo3             | $test3 $test3_u | $test3_retrans"
 echo "---------------------------------"
 
-
 # Remove temporary files
-rm /tmp/cubic_results.txt /tmp/bbr_results.txt /tmp/bbrw_results.txt /tmp/bbry_results.txt /tmp/bbrx_results.txt
-
-
+rm /tmp/${Algo1}_results.txt /tmp/${Algo2}_results.txt /tmp/${Algo3}_results.txt
