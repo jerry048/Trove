1f0da5fefee714538595d41006a3759d064b0870
Jerry048 <jw950@jerry048.com>
2025-01-29 03:46:19 +0800

    Bugfix

--- Vaultwarden-BackUP/.vaultwarden_backup.sh
+++ Vaultwarden-BackUP/.vaultwarden_backup.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
+if [ "$(docker container inspect -f '{{.State.Status}}' vaultwarden)" != "exited" ]; then
+    exit 0
+fi
+
 # Variables
 BACKUP_DIR="/vw-data-backups" # Backup directory where backups are stored
 VW_DATA="/vw-data" # Directory to restore Vaultwarden data
 BACKUP_RETAIN_DAYS=14 # Number of days to keep backups
-
-# Stop Vaultwarden service
-docker container stop vaultwarden
-i=0
-while [ "$(docker container inspect -f '{{.State.Status}}' vaultwarden)" != "exited" ]; do
-    sleep 1
-    i=$((i+1))
-    if [ $i -gt 10 ]; then
-        echo "Failed to stop Vaultwarden service. Exiting..." 1>&2
-        exit 1
-    fi
-done
 
 # Clean up old Vaultwarden data
 if [ ! -d "$BACKUP_DIR" ]; then
@@ -37,9 +29,6 @@ cp "$BACKUP_DIR/$latest_backup/config.json" "$VW_DATA/"
 cp "$BACKUP_DIR/$latest_backup/rsa_key.pem" "$VW_DATA/"
 cp -r "$BACKUP_DIR/$latest_backup/icon_cache" "$VW_DATA/"
 cp "$BACKUP_DIR/$latest_backup/db.sqlite3" "$VW_DATA/"
-
-# Start Vaultwarden service
-docker container start vaultwarden
 
 # Clean up old backups
 find "$BACKUP_DIR" -type d -mtime +$BACKUP_RETAIN_DAYS -exec rm -rf {} \;
--- Vaultwarden-BackUP/.vaultwarden_status.sh
+++ Vaultwarden-BackUP/.vaultwarden_status.sh
@@ -4,45 +4,55 @@
 DOMAIN="example.com"
 CF_API_KEY="YOUR_API_KEY"
 CF_EMAIL="YOUR_EMAIL"
-CADDYFILE="/etc/caddy/Caddyfile"
-VW_PORT=8080
+MAIN_IP="YOUR_MAIN_IP"
+Vaultwarden_DOMAIN="vault.example.com"
 
 CF_ZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$DOMAIN" \
-    -H "Authorization: Bearer $ CF_API_KEY" \
+    -H "Authorization: Bearer $CF_API_KEY" \
     -H "Content-Type: application/json" | jq -r '.result[0].id')
-CF_RECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/dns_records?type=A&name=$DOMAIN" \
-    -H "Authorization: Bearer $ CF_API_KEY" \
+CF_RECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/dns_records?type=A&name=$Vaultwarden_DOMAIN" \
+    -H "Authorization: Bearer $CF_API_KEY" \
     -H "Content-Type: application/json" | jq -r '.result[0].id')
 
 # Get the backup IP address
 BACKUP_IP=$(curl -s https://api.ipify.org)
+if [[ $MAIN_IP == *":"* ]]; then
+    MAIN_IP_TYPE="AAAA"
+else
+    MAIN_IP_TYPE="A"
+fi
 if [[ $BACKUP_IP == *":"* ]]; then
     BACKUP_IP_TYPE="AAAA"
 else
     BACKUP_IP_TYPE="A"
 fi
 
-# Check if the main Vaultwarden URL is down
-if ! curl --silent --head "https://$DOMAIN" | grep "200" > /dev/null; then
-    # Update the DNS record to point to the backup IP address
-    curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/dns_records/$CF_RECORD_ID" \
-        -H "Authorization: Bearer $
-        CF_API_KEY" \
-        -H "Content-Type: application/json" \
-        --data "{\"type\":\"$BACKUP_IP_TYPE\",\"name\":\"$DOMAIN\",\"content\":\"$BACKUP_IP\",\"ttl\":1,\"proxied\":true}"
-    # Start Cadddy
-    systemctl start caddy
+# Check if the main Vaultwarden URL is up
+if [ "$(docker container inspect -f '{{.State.Status}}' vaultwarden)" != "exited" ]; then
+    if curl --silent --head "https://$Vaultwarden_DOMAIN" | grep "200" > /dev/null; then
+        # Update the DNS record to point to the main IP address
+        curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/dns_records/$CF_RECORD_ID" \
+            -H "Authorization: Bearer $CF_API_KEY" \
+            -H "Content-Type: application/json" \
+            --data "{\"type\":\"$MAIN_IP_TYPE\",\"name\":\"$Vaultwarden_DOMAIN\",\"content\":\"$MAIN_IP\",\"ttl\":1,\"proxied\":true}"
+        # Stop Caddy & Vaultwarden
+        systemctl stop caddy
+        docker container stop vaultwarden
+    fi
+    exit 0
 fi
 
-# Check if the main Vaultwarden URL is up
-if curl --silent --head "https://$DOMAIN" | grep "200" > /dev/null; then
-    # Update the DNS record to point to the main IP address
+# Check if the main Vaultwarden URL is down
+if ! curl --silent --head "https://$Vaultwarden_DOMAIN" | grep "200" > /dev/null; then
+    # Update the DNS record to point to the backup IP address
     curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/dns_records/$CF_RECORD_ID" \
-        -H "Authorization: Bearer $
-        CF_API_KEY" \
+        -H "Authorization: Bearer $CF_API_KEY" \
         -H "Content-Type: application/json" \
-        --data "{\"type\":\"$BACKUP_IP_TYPE\",\"name\":\"$DOMAIN\",\"content\":\"$MAIN_IP\",\"ttl\":1,\"proxied\":true}"
-    # Stop Caddy
-    systemctl stop caddy
+        --data "{\"type\":\"$BACKUP_IP_TYPE\",\"name\":\"$Vaultwarden_DOMAIN\",\"content\":\"$BACKUP_IP\",\"ttl\":1,\"proxied\":true}"
+    # Start Cadddy & Vaultwarden
+    systemctl start caddy
+    docker container start vaultwarden
+    exit 0
 fi
+
 
